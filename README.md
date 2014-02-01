a536231d1ad7690c3620135a01777287d0e9e0d1
inaimathi <inaimathi@self>
2014-02-01 15:04:12 -0500

    -Starting on parts-of-speech passphrase generation

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Humane Auth
 
-Common Lisp implementation of the authentication system proposed in Raskin's "The Humane Interface"
+Common Lisp implementation of the authentication system proposed in Raskin's ["The Humane Interface"](http://www.amazon.com/The-Humane-Interface-Directions-Interactive/dp/0201379376).
+
+A writeup of the thought process can be found [here](http://langnostic.blogspot.ca/2013/12/implementing-humane-authentication.html).
+
+Includes a mildly modified version of the Parts of Speech database from [here](http://wordlist.sourceforge.net/) (thank you, Kevin Atkinson).
--- util.lisp
+++ util.lisp
@@ -1,5 +1,45 @@
 (in-package #:humane-auth)
 
+;;;; Part of Speech DB
+(defparameter char->label
+  (list #\N :noun
+	#\p :plural
+	#\h :noun-phrase
+	#\V :verb
+	#\t :transitive-verb
+	#\i :intransitive-verb
+	#\A :adjective
+	#\v :adverb
+	#\C :conjunction
+	#\P :preposition
+	#\! :interjection
+	#\r :pronoun
+	#\D :definite-article
+	#\I :indefinite-article
+	#\o :nominative))
+
+(defparameter parts-of-speech (make-hash-table))
+
+(defun line->entry! (line)
+  (destructuring-bind (word labels) (split-sequence #\tab line)
+    (loop for l across labels
+       do (push word (gethash (getf char->label l) parts-of-speech)))))
+
+(with-open-file (s "part-of-speech.txt")
+  (loop for ln = (read-line s nil nil)
+     while ln do (line->entry! ln)))
+
+(loop for k being the hash-keys of parts-of-speech
+   for v being the hash-values of parts-of-speech
+   do (setf (gethash k parts-of-speech) (coerce v 'vector)))
+
+(defun pick (type)
+  (let ((words (gethash type parts-of-speech)))
+    (aref words (random (length words)))))
+
+(defun phrase (word-types)
+  (loop for w in word-types collect (pick w)))
+
 ;;;; Dictionary-related
 (defconstant +dict+ (coerce 
                      (with-open-file (s "/usr/share/dict/american-english")
